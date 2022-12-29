30c99e63b11bb64cfbefe492bf89461632a63498
Marco Antonio Gonzalez Castelan <marcoantonigoncaas@gmail.com>
2022-12-28 21:20:11 -0600

    Pagelist creado

---
+++ 095-Math/PageList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _095_Math
+{
+    public class PageList<T>:IEnumerable<T>
+    {
+
+        public PageList(IEnumerable<T> enumElementos,int elementosPorPagina) {
+
+            this.enumElementos = enumElementos;
+            
+            this.elementosPorPagina = elementosPorPagina;
+            this.totalElementos = enumElementos.Count();
+            this.totalPaginas = CalcularTotalPaginas(totalElementos, elementosPorPagina);
+            this.paginaActual = 1;
+
+        }
+        
+
+        #region Helpers
+        private int CalcularTotalPaginas(int totalElementos,int elementosPorPagina)
+        {
+            return (int)Math.Ceiling((double)totalElementos / (double)elementosPorPagina);
+        }
+
+#endregion
+        public IEnumerable<T> ObtenerPaginado(int pagina)
+        {
+            if(pagina < 1 || pagina>this.totalPaginas)
+            {
+                throw new IndexOutOfRangeException(nameof(pagina));
+            }    
+
+            int elementosPasado = (pagina - 1) * elementosPorPagina;
+
+            return this.Skip(elementosPasado).Take(elementosPorPagina);
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return enumElementos.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return enumElementos.GetEnumerator();
+        }
+
+
+        private int totalPaginas;
+        private int paginaActual;
+        private int elementosPorPagina;
+        private int totalElementos;
+        private IEnumerable<T> enumElementos;
+
+
+
+        public int TotalPaginas
+        {
+            get => totalPaginas;
+        }
+
+        public int PaginaActual
+        {
+            get => paginaActual;
+        }
+
+
+    }
+
+    public static class PageListExtensio
+    {
+        public static PageList<T> ToPageList<T>(this IEnumerable<T> enumerable,int elementosPorPagina)
+        {
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException(nameof(enumerable));
+            }
+            if (elementosPorPagina < 1)
+            {
+               throw new IndexOutOfRangeException($"El numeor de elementos no puede ser menor a 1 {nameof(elementosPorPagina)}");
+            }
+
+
+
+
+            var page = new PageList<T>(enumerable, elementosPorPagina);
+
+
+            return page;
+        }
+    }
+}
---
+++ 095-Math/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _095_Math
+{
+    internal class Program
+    {
+        static void LlenarEnum(List<int> enumerable,int min,int max)
+        {
+            for(int i=min;i<=max;i++)
+            {
+                enumerable.Add(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+
+
+            LlenarEnum(list, 1, 101);
+
+
+
+            PageList<int> listPaginada = list.ToPageList(10);
+            
+            int pagina;
+            
+
+            do
+            {
+                Console.WriteLine("0.- Salir");
+
+
+                for (int i = 0; i < listPaginada.TotalPaginas; i++)
+                {
+                    Console.WriteLine($"Pagina {i + 1}");
+                }
+                Console.Write("Selecciona la pagina: ");
+                pagina = Convert.ToInt32(Console.ReadLine());
+
+                if(pagina >0&& pagina <= listPaginada.TotalPaginas)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\tMostrando paginado\n");
+                    List<int> lstCortada = listPaginada.ObtenerPaginado(pagina).ToList();
+                    lstCortada.ForEach(item =>
+                    {
+                        Console.WriteLine(item);
+                    });
+                    Console.WriteLine("\n=======================\n");
+                }
+
+            } while (pagina != 0);
+
+
+        }
+    }
+}
